000fe82de52573da6aba6e591d0fe0a8fb55698d
Dimitri Lagast <lagast.dimitri@gmail.com>
2026-01-22 07:43:47 +0100

    setup gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ dependencies {
 hytale {
     // uncomment if you want to add the Assets.zip file to your external libraries;
     // ⚠️ CAUTION, this file is very big and might make your IDE unresponsive for some time!
-    //
-    // addAssetsDependency = true
+
+     addAssetsDependency = true
 
     // uncomment if you want to develop your mod against the pre-release version of the game.
     //
