3181cab2cf6b38bc2391e1db8d73fe5adbf74929
Dimitri Lagast <lagast.dimitri@gmail.com>
2026-01-26 06:47:02 +0100

    WIP colony system

--- src/main/java/com/colonies/coloniesplugin/ColoniesPlugin.java
+++ src/main/java/com/colonies/coloniesplugin/ColoniesPlugin.java
@@ -1,11 +1,19 @@
 package com.colonies.coloniesplugin;
 
+import com.colonies.coloniesplugin.components.ColonistComponent;
+import com.colonies.coloniesplugin.systems.ColonySystem;
+import com.hypixel.hytale.component.ComponentType;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 public class ColoniesPlugin extends JavaPlugin {
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    private static ColoniesPlugin instance;
+
+    private ComponentType<EntityStore, ColonistComponent> colonistComponent;
 
     public ColoniesPlugin(JavaPluginInit init) {
         super(init);
@@ -14,6 +22,13 @@ public class ColoniesPlugin extends JavaPlugin {
 
     @Override
     protected void setup() {
+        // Commands
         this.getCommandRegistry().registerCommand(new ColoniesCommand(this.getName(), this.getManifest().getVersion().toString()));
+
+        // Components
+        this.colonistComponent = this.getEntityStoreRegistry().registerComponent(ColonistComponent.class, ColonistComponent::new);
+
+        // Systems
+        this.getEntityStoreRegistry().registerSystem(new ColonySystem(this.colonistComponent));
     }
 }
