4504296f0f8d84948b0bc167ba9e412167195976
Dimitri Lagast <lagast.dimitri@gmail.com>
2026-02-10 23:06:04 +0100

    Figured out initial setup of job systems.

--- src/main/java/com/colonies/coloniesplugin/components/jobs/JobProviderComponent.java
+++ src/main/java/com/colonies/coloniesplugin/components/jobs/JobProviderComponent.java
@@ -1,5 +1,8 @@
 package com.colonies.coloniesplugin.components.jobs;
 
+import com.hypixel.hytale.codec.Codec;
+import com.hypixel.hytale.codec.KeyedCodec;
+import com.hypixel.hytale.codec.builder.BuilderCodec;
 import com.hypixel.hytale.component.Component;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
 import org.jspecify.annotations.Nullable;
@@ -8,6 +11,14 @@ import org.jspecify.annotations.Nullable;
 /// Component for entities that provide jobs to colonists.
 /// </summary>
 public class JobProviderComponent implements Component<ChunkStore> {
+
+    public static final BuilderCodec<JobProviderComponent> CODEC = BuilderCodec.builder(JobProviderComponent.class, JobProviderComponent::new)
+            .append(new KeyedCodec<>("JobType", Codec.STRING), (jobProviderComponent, s) -> jobProviderComponent.JobType = s, jobProviderComponent -> jobProviderComponent.JobType)
+            .add()
+            .build();
+
+    public String JobType;
+
     public JobProviderComponent() {
     }
 
--- src/main/java/com/colonies/coloniesplugin/systems/jobs/JobAssignmentSystem.java
+++ src/main/java/com/colonies/coloniesplugin/systems/jobs/JobAssignmentSystem.java
@@ -1,13 +1,13 @@
 package com.colonies.coloniesplugin.systems.jobs;
 
 import com.colonies.coloniesplugin.ColoniesPlugin;
+import com.colonies.coloniesplugin.components.npc.ColonistComponent;
 import com.hypixel.hytale.component.*;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.tick.DelayedEntitySystem;
-import com.hypixel.hytale.server.core.modules.block.BlockModule;
-import com.hypixel.hytale.server.core.universe.world.meta.state.LaunchPad;
-import com.hypixel.hytale.server.core.universe.world.meta.state.RespawnBlock;
+import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
@@ -18,12 +18,11 @@ public class JobAssignmentSystem extends DelayedEntitySystem<ChunkStore> {
     // Query for NPCs that have a ColonistComponent but NO JobLink (unemployed)
 //    private final Query<EntityStore> unemployedQuery;
     // Query for Job Sources (Workstations/Blocks)
-    private final Query<ChunkStore> jobProviderQuery;
+    private final Query<ChunkStore> jobProviderQuery = Archetype.of(ColoniesPlugin.getInstance().getJobProviderComponentType());
+    private final Query<EntityStore> unemployedQuery = Archetype.of(ColoniesPlugin.getInstance().getColonistComponentType());
 
     public JobAssignmentSystem() {
         super(1.0f); // Run once every 1 second.
-
-        jobProviderQuery = Archetype.of(ColoniesPlugin.getInstance().getJobProviderComponentType());
     }
 
     @Override
@@ -32,29 +31,29 @@ public class JobAssignmentSystem extends DelayedEntitySystem<ChunkStore> {
                      @NonNull Store<ChunkStore> store,
                      @NonNull CommandBuffer<ChunkStore> commandBuffer)
     {
-//        ColoniesPlugin.LOGGER.atInfo().log(
-//                String.format(
-//                        "JobAssignmentSystem #%d Tick: Scanning for unemployed colonists in chunk: %s",
-//                        index, archetypeChunk.toString()
-//                )
-//        );
+        ColoniesPlugin.LOGGER.atInfo().log(String.format("JobAssignmentSystem JobProvider #%d Tick: Looking for unemployed colonists.", index));
 
-        // For each unemployed colonist, log their info (ToDo: Assign job)
-//        store.forEachEntityParallel(unemployedQuery, (id, _archetypeChunk, _commandBuffer) -> {
-//            ColonistComponent colonist = archetypeChunk.getComponent(id, ColoniesPlugin.getInstance().getColonistComponentType());
-//            ColoniesPlugin.LOGGER.atInfo().log(
-//                    String.format(
-//                            "Assigning job to colonist #%d : %s | Colonist info: %s | Chunk: %s",
-//                            id, colonist.getColonistName(), colonist.toString(), archetypeChunk.toString()
-//                    )
-//            );
-//        });
+        World world = store.getExternalData().getWorld();
+        EntityStore entityStore = world.getEntityStore();
+
+        entityStore.getStore().forEachEntityParallel(unemployedQuery, (id, _archetypeChunk, _cb) -> {
+            ColonistComponent colonist = _archetypeChunk.getComponent(id, ColoniesPlugin.getInstance().getColonistComponentType());
+            if (colonist == null) {
+                ColoniesPlugin.LOGGER.atInfo().log(String.format("Colonist #%d : ColonistComponent is NULL", id));
+                return;
+            }
+
+            if (colonist.isEmployed()) {
+                ColoniesPlugin.LOGGER.atInfo().log(String.format("Colonist #%d : %s is EMPLOYED | Colonist info: %s", id, colonist.getColonistName(), colonist));
+            } else {
+                ColoniesPlugin.LOGGER.atInfo().log(String.format("Colonist #%d : %s is UNEMPLOYED | Colonist info: %s", id, colonist.getColonistName(), colonist));
+            }
+        });
     }
 
 
     @Override
     public @Nullable Query<ChunkStore> getQuery() {
-//        return jobProviderQuery;
-       return Query.and(RespawnBlock.getComponentType());
+        return jobProviderQuery;
     }
 }
