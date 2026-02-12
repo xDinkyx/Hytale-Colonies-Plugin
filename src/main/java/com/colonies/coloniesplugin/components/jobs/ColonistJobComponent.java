13e44aab90f98704d6c90b4c86e5102d869b55de
Dimitri Lagast <lagast.dimitri@gmail.com>
2026-02-12 18:06:36 +0100

    WIP figuring out job assignment

--- src/main/java/com/colonies/coloniesplugin/components/jobs/ColonistJobComponent.java
+++ src/main/java/com/colonies/coloniesplugin/components/jobs/ColonistJobComponent.java
@@ -7,16 +7,18 @@ import com.hypixel.hytale.component.Component;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.jspecify.annotations.Nullable;
 
+import java.util.UUID;
+
 public class ColonistJobComponent implements Component<EntityStore> {
 
     public static final BuilderCodec<ColonistJobComponent> CODEC = BuilderCodec.builder(ColonistJobComponent.class, ColonistJobComponent::new)
-            .append(new KeyedCodec<>("JobProviderEntityIndex", Codec.INTEGER), (colonistJobComponent, s) -> colonistJobComponent.JobProviderEntityIndex = s, colonistJobComponent -> colonistJobComponent.JobProviderEntityIndex)
+            .append(new KeyedCodec<>("JobProviderEntityIndex", Codec.UUID_STRING), (colonistJobComponent, s) -> colonistJobComponent.JobProviderEntityUUID = s, colonistJobComponent -> colonistJobComponent.JobProviderEntityUUID)
             .add()
             .append(new KeyedCodec<>("CurrentTask", Codec.STRING), (colonistJobComponent, s) -> colonistJobComponent.CurrentTask = s, colonistJobComponent -> colonistJobComponent.CurrentTask)
             .add()
             .build();
 
-    public int JobProviderEntityIndex; // Index of JobProvider component this colonist is currently working for.
+    public UUID JobProviderEntityUUID; // UUID of JobProvider component this colonist is currently working for.
     public String CurrentTask = "IDLE"; // TRAVELING, WORKING, REFUELING // ToDo: Make this an enum?
 
     public ColonistJobComponent() {}
@@ -24,7 +26,7 @@ public class ColonistJobComponent implements Component<EntityStore> {
     @Override
     public @Nullable Component<EntityStore> clone() {
         ColonistJobComponent copy = new ColonistJobComponent();
-        copy.JobProviderEntityIndex = this.JobProviderEntityIndex;
+        copy.JobProviderEntityUUID = this.JobProviderEntityUUID;
         copy.CurrentTask = this.CurrentTask;
         return copy;
     }
