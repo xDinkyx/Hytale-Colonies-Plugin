952c561a0aec9363b0e87709322fd8a4644daf98
Dimitri Lagast <lagast.dimitri@gmail.com>
2026-02-24 22:48:20 +0100

    Fix and improve path finding by using path manager.

--- src/main/java/com/colonies/coloniesplugin/systems/npc/PathFindingSystem.java
+++ src/main/java/com/colonies/coloniesplugin/systems/npc/PathFindingSystem.java
@@ -1,6 +1,10 @@
 package com.colonies.coloniesplugin.systems.npc;
 
-import com.colonies.coloniesplugin.components.npc.ColonistComponent;
+import javax.annotation.Nonnull;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import com.colonies.coloniesplugin.ColoniesPlugin;
 import com.colonies.coloniesplugin.components.npc.MoveToTargetComponent;
 import com.hypixel.hytale.component.ArchetypeChunk;
 import com.hypixel.hytale.component.CommandBuffer;
@@ -8,68 +12,88 @@ import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.tick.EntityTickingSystem;
-import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.math.vector.Transform;
 import com.hypixel.hytale.math.vector.Vector3d;
+import com.hypixel.hytale.server.core.entity.UUIDComponent;
+import com.hypixel.hytale.server.core.modules.debug.DebugUtils;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.path.WorldPath;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.npc.entities.NPCEntity;
-import com.hypixel.hytale.builtin.path.path.TransientPath;
-import java.util.ArrayDeque;
-import com.hypixel.hytale.builtin.path.waypoint.RelativeWaypointDefinition;
-import org.jspecify.annotations.NonNull;
-import org.jspecify.annotations.Nullable;
+import com.hypixel.hytale.server.npc.entities.PathManager;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
+/**
+ * A one-shot system that runs when an entity has a {@link MoveToTargetComponent}.
+ * It calculates a path to the target and assigns it to the NPC's {@link PathManager},
+ * then removes the component to prevent running again.
+ * ToDo:
+ * - Maybe this is not the correct type of system. Its probably better to use a RefChangeSystem<> that triggers when MoveToTargetComponent is added. 
+ *   This way we can guarantee it runs immediately when the component is added, instead of waiting for the next tick.
+ *   Alternatively we keep as is. A path is calculated when changing job state. This way we can ignore world changes and optimizations.
+ * - Stop NPC following old path when new one is assigned. Now it first completes its path before following the new one.
+ * - Store the calculated path. Reload path instead of recalculating. 
+ * - Optimize pathfinding by only recalculating when necessary (e.g. target moved, path blocked, blocks changed, etc.). 
+ */
 public class PathFindingSystem extends EntityTickingSystem<EntityStore> {
-
-    private static final HytaleLogger logger = HytaleLogger.forEnclosingClass();
 
     public PathFindingSystem() {
     }
 
     @Override
-    public void tick(float value,
-                     int index,
-                     @NonNull ArchetypeChunk<EntityStore> archetypeChunk,
-                     @NonNull Store<EntityStore> store,
-                     @NonNull CommandBuffer<EntityStore> commandBuffer) {
-        MoveToTargetComponent moveToTargetComponent = archetypeChunk.getComponent(index, MoveToTargetComponent.getComponentType());
-        assert moveToTargetComponent != null;
-        Vector3d target = moveToTargetComponent.target;
+    public void tick(
+            float value,
+            int index,
+            @NonNull ArchetypeChunk<EntityStore> archetypeChunk,
+            @NonNull Store<EntityStore> store,
+            @NonNull CommandBuffer<EntityStore> commandBuffer) {
 
-        // Get ref to the colonist entity.
-        Ref<EntityStore> colonistRef = archetypeChunk.getReferenceTo(index);
+        Ref<EntityStore> entityRef = archetypeChunk.getReferenceTo(index);
 
-        // Remove the MoveToTargetComponent immediately to prevent this system from running on this entity again.
-        commandBuffer.removeComponent(colonistRef, MoveToTargetComponent.getComponentType());
+        // Get relevant components.
+        MoveToTargetComponent moveTo = store.getComponent(entityRef, MoveToTargetComponent.getComponentType());
+        TransformComponent transformComponent = store.getComponent(entityRef, TransformComponent.getComponentType());
+        NPCEntity npcEntity = store.getComponent(entityRef, NPCEntity.getComponentType());
+        UUIDComponent uuidComponent = store.getComponent(entityRef, UUIDComponent.getComponentType());
 
-        TransformComponent transform = archetypeChunk.getComponent(index, TransformComponent.getComponentType());
-        assert transform != null;
+        // This is a one-shot system. Remove the component immediately so we don't process this entity again.
+        commandBuffer.removeComponent(entityRef, MoveToTargetComponent.getComponentType());
 
-        NPCEntity npc = archetypeChunk.getComponent(index, NPCEntity.getComponentType());
-        assert npc != null;
+        if (moveTo == null || transformComponent == null || npcEntity == null || uuidComponent == null) {
+            ColoniesPlugin.LOGGER.atWarning().log(String.format("Entity %s is missing required components for PathFindingSystem", index));
+            return;
+        }
 
-        logger.atInfo().log("Assigning direct path for NPC from %s to %s", transform.getPosition(), target);
+        Vector3d from = transformComponent.getTransform().getPosition();
+        Vector3d to = moveTo.target;
 
-        // Build a direct path using a single relative waypoint (angle and distance)
-        Vector3d from = transform.getPosition();
-        Vector3d to = target;
-        double dx = to.x - from.x;
-        double dz = to.z - from.z;
-        double distance = Math.sqrt(dx * dx + dz * dz);
-        float angle = (float) Math.atan2(dz, dx);
+        // Create a new WorldPath object with the start and end points.
+        // This path is "transient" and only exists for this movement.
+        ObjectArrayList<Transform> waypoints = new ObjectArrayList<>();
+        waypoints.add(transformComponent.getTransform().clone()); // Current location
+        waypoints.add(new Transform(to)); // Target location
+        WorldPath transientPath = new WorldPath("transient-path-" + uuidComponent.getUuid(), waypoints);
 
-        ArrayDeque<RelativeWaypointDefinition> instructions = new ArrayDeque<>();
-        instructions.add(new RelativeWaypointDefinition(angle, distance));
+        // Get the NPC's PathManager and assign the new transient path.
+        PathManager pathManager = npcEntity.getPathManager();
+        if (pathManager != null) {
+            pathManager.setTransientPath(transientPath);
+        }
 
-        // Use the NPC's current head rotation if available, else zero
-        // ToDo: (If you want to use head rotation, you can get HeadRotation component here)
-        npc.getPathManager().setTransientPath(
-            TransientPath.buildPath(from, transform.getRotation(), instructions, 1.0)
-        );
+        // Display debug shapes to visualize the path for 20 seconds.
+        showDebugPath(store.getExternalData().getWorld(), from, to);
     }
 
     @Override
     public @Nullable Query<EntityStore> getQuery() {
+        // This system will only run on entities that have the MoveToTargetComponent.
         return Query.and(MoveToTargetComponent.getComponentType());
+    }
+
+    private void showDebugPath(@Nonnull World world, @Nonnull Vector3d from, @Nonnull Vector3d to) {
+        DebugUtils.addSphere(world, from, DebugUtils.COLOR_BLUE, 0.5, 20.0f);
+        DebugUtils.addSphere(world, to, DebugUtils.COLOR_RED, 0.5, 20.0f);
+        DebugUtils.addLine(world, from, to, DebugUtils.COLOR_GREEN, 0.1, 20.0f, true);
     }
 }
